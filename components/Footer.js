ebf4dcb498dce4935156d4189078f3631d647717
fjjd <thiagoluna2@gmail.com>
2020-04-13 09:53:06 -0300

    Correção Link Linkedin

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ const Footer = () => {
     return (
         <div className='py-4 text-center bg-gray-400'>
             Project created during Fullstack Lab of DevPleno.<br/>
-            Implemented by <a href="https://kinkedin.com/in/thiagoluna">Thiago Luna</a>
+            Implemented by Thiago Luna <br/>
+            <a href="https://www.linkedin.com/in/thiago-luna/" target="_blank">Linkedin</a> - <a href="https://github.com/thiagoluna" target="_blank">GitHub</a>
         </div>  
     )
 }
